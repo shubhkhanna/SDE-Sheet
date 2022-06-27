fc12d82e99b47c6a423166b5a9d313cb96ec65c4
Shubh Khanna <1000012859@dit.edu.in>
2022-06-28 01:33:36 +0530

    Time: 119 ms (91.12%), Space: 67.7 MB (53.28%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int meh = nums[0];
+        
+        for(int i=0;i<size(nums);i++){
+            sum += nums[i];
+            
+            if(sum > meh){
+                meh = sum;
+            }
+            
+            if(sum < 0) sum = 0;
+        }
+        
+        return meh;
+    }
+};
