e56b2c245c2541a87507730dbe0fd5b17dc28f8d
Shubh Khanna <1000012859@dit.edu.in>
2022-07-28 08:21:36 +0530

    Time: 7 ms (87.80%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size() != t.size()) return false;
+        
+        vector<int> ans(26,0);
+        
+        for(int i=0;i<s.size();i++){
+            ans[s[i]-'a']++;
+            ans[t[i]-'a']--;
+        }
+        
+        for(auto x:ans){
+            if (x) return false;
+        }
+        
+        return true;
+    }
+};
