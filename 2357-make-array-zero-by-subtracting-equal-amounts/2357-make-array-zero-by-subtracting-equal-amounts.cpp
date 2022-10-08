ffe9317faeb190ba7b76b820e28dbc9693b8ddac
Shubh Khanna <1000012859@dit.edu.in>
2022-10-08 17:13:55 +0530

    Time: 19 ms (7.04%), Space: 9.6 MB (13.12%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();                    
+       
+        int count = 0;                
+    
+        for(int i = 0; i < n; i++){
+            priority_queue<int,vector<int>,greater<int>> p;
+            
+            for(int i=0;i<n;i++) {
+                if(nums[i] != 0) p.push(nums[i]);
+            }
+            
+            cout << p.size() << " ";
+            
+            if(!p.empty()) {
+                count++;                
+            } else {
+                break;
+            }
+            
+            int top = p.top();
+            
+            for(int j=0;j<n;j++){
+                if(nums[j]!=0) {                
+                   nums[j] -= top;             
+                }    
+            }
+        }                
+        return n == 0 ? 0 : count;
+    }
+};
